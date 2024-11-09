3655ac0f1c70a67e8377699702b2c27a5839a554
KolodkinGrigorii <113025092+KolodkinGrigorii@users.noreply.github.com>
2024-11-09 13:40:15 +0300

    Debug4

--- tasks/mpi/kolodkin_g_image_contrast/src/ops_mpi.cpp
+++ tasks/mpi/kolodkin_g_image_contrast/src/ops_mpi.cpp
@@ -21,8 +21,15 @@ bool kolodkin_g_image_contrast_mpi::TestMPITaskSequential::pre_processing() {
 
 bool kolodkin_g_image_contrast_mpi::TestMPITaskSequential::validation() {
   internal_order_test();
-  if (taskData->inputs_count[0] % 3 != 0) {
+  if (taskData->inputs_count[0] <= 0 || taskData->inputs_count[0] % 3 != 0) {
     return false;
+  }
+  auto* input_ptr = reinterpret_cast<int*>(taskData->inputs[0]);
+  for (unsigned long i = 0; i < taskData->inputs_count[0]; i++) {
+    if (*input_ptr > 255 || *input_ptr < 0) {
+      return false;
+    }
+    input_ptr++;
   }
   return true;
 }
@@ -86,8 +93,17 @@ bool kolodkin_g_image_contrast_mpi::TestMPITaskParallel::pre_processing() {
 
 bool kolodkin_g_image_contrast_mpi::TestMPITaskParallel::validation() {
   internal_order_test();
-  if (taskData->inputs_count[0] % 3 != 0) {
-    return false;
+  if (world.rank() == 0) {
+    if (taskData->inputs_count[0] <= 0 || taskData->inputs_count[0] % 3 != 0) {
+      return false;
+    }
+    auto* input_ptr = reinterpret_cast<int*>(taskData->inputs[0]);
+    for (unsigned long i = 0; i < taskData->inputs_count[0]; i++) {
+      if (*input_ptr > 255 || *input_ptr < 0) {
+        return false;
+      }
+      input_ptr++;
+    }
   }
   return true;
 }
