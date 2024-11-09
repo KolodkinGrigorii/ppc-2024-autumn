3451fa6cb60f4fe69b4fe28df8c3da3f0da87f5e
KolodkinGrigorii <113025092+KolodkinGrigorii@users.noreply.github.com>
2024-11-09 14:21:27 +0300

    Debug7

--- tasks/mpi/kolodkin_g_image_contrast/func_tests/main.cpp
+++ tasks/mpi/kolodkin_g_image_contrast/func_tests/main.cpp
@@ -212,8 +212,8 @@ TEST(kolodkin_g_image_contrast_MPI, Test_global_and_reference) {
     testTaskSequential.pre_processing();
     testTaskSequential.run();
     testTaskSequential.post_processing();
-    global_out = *reinterpret_cast<std::vector<int> *>(taskDataMpi->outputs[0]);
-    reference_out = *reinterpret_cast<std::vector<int> *>(taskDataSeq->outputs[0]);
+    //global_out = *reinterpret_cast<std::vector<int> *>(taskDataMpi->outputs[0]);
+    //reference_out = *reinterpret_cast<std::vector<int> *>(taskDataSeq->outputs[0]);
   }
 }
 
